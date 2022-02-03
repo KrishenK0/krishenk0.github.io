751c11163cc7a28a7c099fc2ed581bd791571e19
KrishenK <fox2581kriskenk@gmail.com>
2022-02-03 22:34:01 +0100

    Darkmode colors update, shell need fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let shell = `[c color="gold"]KrishenK Portfolio[/c] [version 10.0.19043.1466]
+let shell = `[c color="orange"]KrishenK Portfolio[/c] [version 10.0.19043.1466]
 (c) KrishenK Corporation. All right reserved.
 
 type "help", "copyright", "credits" or "license" for more information.\n`;
@@ -26,12 +26,12 @@ let decode = (text) => {
 
     ];
     let result = [
-        '<span style="color:$1;">$2</span>',
+        '<span style="color:var(--$1, $1);">$2</span>',
     ];
     for (let row of rows) {
         if (group = row.matchAll(new RegExp(regex.join(''), 'gi'))) {
             [...group].forEach(match => {
-                row = row.replace('[' + match[0], result[0].replace('$1', match[2].slice(1, -1)).replace('$2', match[3].slice(0, -2)));
+                row = row.replace('[' + match[0], result[0].replaceAll('$1', match[2].slice(1, -1)).replace('$2', match[3].slice(0, -2)));
             });
         }
         decoded += row + '\n';
